c5f58de48b4f3c8dff2eb81d3a1f0ef7646ee64b
merhaxxey <94338413+merhaxxey@users.noreply.github.com>
2022-12-02 12:39:18 +0100

    Delete easyBuy-backend directory

--- easyBuy-backend/errors/not-found.js
+++
@@ -1,9 +0,0 @@
-const CustomAPIError = require('./custom-api')
-const {StatusCodes} = require('http-status-codes')
-
-class NotFound extends CustomAPIError{
-    constructor(message){
-        super(message, StatusCodes.NOT_FOUND)
-    }
-}
-module.exports = NotFound
