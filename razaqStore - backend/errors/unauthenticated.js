fad23f73f5d7578a7d54efed2eb8681a80131ec9
merhaxxey <94338413+merhaxxey@users.noreply.github.com>
2022-12-12 09:20:07 +0100

    Delete razaqStore - backend directory

--- razaqStore - backend/errors/unauthenticated.js
+++
@@ -1,9 +0,0 @@
-const CustomAPIError = require('./custom-api')
-const {StatusCodes} = require('http-status-codes')
-
-class Unauthenticated extends CustomAPIError{
-    constructor(message){
-        super(message, StatusCodes.UNAUTHORIZED)
-    }
-}
-module.exports = Unauthenticated
